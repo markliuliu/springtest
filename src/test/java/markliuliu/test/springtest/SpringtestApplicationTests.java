e721307a3858224c3ae9951349bfd9694dbe6b39
markliuliu <markliu0000@gmail.com>
2019-03-13 23:29:43 +0800

    first commit for spring boot

---
+++ src/test/java/markliuliu/test/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,16 @@
+package markliuliu.test.springtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
